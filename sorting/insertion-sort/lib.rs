c40add2fb00c5eaa7a55a5283b414a3a311847ba
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2022-10-15 12:36:06 -0300

    feat: `insertion-sort`

---
+++ sorting/insertion-sort/lib.rs
@@ -0,0 +1,27 @@
+use sorter::Sorter;
+
+pub struct InsertionSort;
+
+impl Sorter for InsertionSort {
+    fn sort<T: Ord>(&self, xs: &mut [T]) {
+        sort(xs);
+    }
+}
+
+fn sort<T: Ord>(xs: &mut [T]) {
+    for i in 1..xs.len() {
+        let mut prev = i - 1;
+        while &xs[prev] > &xs[prev + 1] {
+            xs.swap(prev, prev + 1);
+            if prev == 0 {
+                break;
+            }
+            prev -= 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod sorter_tests {
+    sorter::test!(super::InsertionSort);
+}
