aa4cb9056572d81d3a11417f3a245cde8945d674
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2022-10-15 12:41:34 -0300

    feat: `selection-sort`

---
+++ sorting/selection-sort/lib.rs
@@ -0,0 +1,26 @@
+use sorter::Sorter;
+
+pub struct SelectionSort;
+
+impl Sorter for SelectionSort {
+    fn sort<T: Ord>(&self, xs: &mut [T]) {
+        sort(xs);
+    }
+}
+
+fn sort<T: Ord>(xs: &mut [T]) {
+    for i in 0..(xs.len() - 1) {
+        let mut min = i;
+        for j in (i + 1)..xs.len() {
+            if &xs[min] > &xs[j] {
+                min = j;
+            }
+        }
+        xs.swap(min, i);
+    }
+}
+
+#[cfg(test)]
+mod sorter_tests {
+    sorter::test!(super::SelectionSort);
+}
